71ac3811ce4da4fe926b7fc595a5e255caaea388
Emergency1999 <johannes@schmid-company.de>
2024-09-29 18:56:16 +0200

    v2.3: updated generate function: - added support for bash execution in template - added with_spaces function

--- func_generate.sh
+++ func_generate.sh
@@ -7,6 +7,26 @@
 # $1: from/template/path.yml
 # $2: to/path.yml
 
+
+with_spaces() {
+  if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters"
+    echo "Remember to put the text in quotes"
+    exit 1
+  fi
+  spaces="$1"
+  text="$2"
+  if ! [[ "$spaces" =~ ^[0-9]+$ ]]; then
+    echo "First argument must be a number"
+    exit 1
+  fi
+  # remove empty lines
+  text=$(echo "$text" | sed '/^$/d')
+  # add spaces on all new lines
+  echo "$text" | sed "s/^/$(printf "%${spaces}s")/"
+}
+export -f with_spaces
+
 generate() {
   local template="$1"
   local generated="$2"
@@ -21,8 +41,22 @@ generate() {
     exit 1
   fi
 
-  echo "Generating $generated"
   local path=$(dirname $generated)
+  local filename=$(basename $generated)
+  echo "Generating $filename"
   mkdir -p $path
-  envsubst <$template >$generated
+
+  local error_output
+  error_output=$( {
+eval "cat <<EOF
+$(<"$template")
+EOF
+" | envsubst > "$generated"; } 2>&1)
+
+  # Check if error_output contains anything
+  if [[ -n "$error_output" ]]; then
+    echo "Error occurred during template generation:"
+    echo "$error_output"
+    exit 1
+  fi
 }
